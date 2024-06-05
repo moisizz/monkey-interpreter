979c1ec757b3768c57fb534258c038b081db0047
Danil Moiseev <danil.moiseev@jetbrains.com>
2024-06-05 19:23:10 +0200

    Add current progress to repository

---
+++ src/monkey/token/token.go
@@ -0,0 +1,60 @@
+package token
+
+type TokenType string
+
+type Token struct {
+    Type TokenType
+    Literal string
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    IDENT = "IDENT"
+    INT = "INT"
+
+    ASSIGN = "="
+    PLUS = "+"
+    BANG = "!"
+    MINUS = "-"
+    SLASH = "/"
+    ASTERISK = "*"
+    LT = "<"
+    GT = ">"
+    EQ = "=="
+    NOT_EQ = "!="
+
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    FUNCTION = "FUNCTION"
+    LET      = "LET"
+    TRUE     = "TRUE"
+    FALSE    = "FALSE"
+    IF       = "IF"
+    ELSE     = "ELSE"
+    RETURN   = "RETURN"
+)
+
+var keywrods = map[string]TokenType{
+    "fn": FUNCTION,
+    "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywrods[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
